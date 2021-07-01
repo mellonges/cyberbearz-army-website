3c879e294d141b5f044afbac1d532d7d7688ed0c
404piano <b89185236417@mail.ru>
2021-07-01 14:08:13 +0300

    add Bears table

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import SimpleAccordion from "./Component/Accordions/accordions";
+import React from "react";
 import Section from "./Component/Bears/Section";
 import NewAccordions from "./Component/Accordions/newAccordions";
-import GetFree from "./Component/Buttons/GetFree";
+import BearsTable from "./Component/Bears Table /BearsTable";
 
 
 function App() {
     return (
         <div className="App">
+            <BearsTable />
             <Section/>
             <NewAccordions/>
         </div>
