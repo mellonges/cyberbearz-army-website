390333619b7db51c4ba65cb0f7e6326f7ce57e10
404piano <b89185236417@mail.ru>
2021-07-05 16:01:10 +0300

    add /squad but dont done logic

---
+++ src/Pages/MySquad.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from "../Navbar/Navbar";
+
+const MySquad = () => {
+    return (
+        <div>
+            <h1>TEST</h1>
+        </div>
+    );
+};
+
+export default MySquad;
